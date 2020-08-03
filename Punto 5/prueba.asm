5c7720b5bb2083fd39503e905f835bf4c24ccf19
JulianVentura <julianv6004@gmail.com>
2020-08-03 18:11:51 -0300

    Agrego lw con pseudo de 4 parametros, parece andar bien. Faltan pruebas

--- Punto 5/prueba.asm
+++ Punto 5/prueba.asm
@@ -1,3 +1,6 @@
 addi $t0,$zero, 2
-addi $t1,$zero, 3
-load $t3, $t0, 10($t1)
+addi $t1,$zero, 4
+addi $t2,$zero, 24
+addi $t3,$zero, 9
+sw $t3, 0($t2)		#M[24] = 9
+load $t6, $t0, 10, $t1 # t6 = M[2*10 + 4]
